3d3012a7769f9cd62cf564ad99b9af38ab0d69c6
Ashiqur Rahman Prince <ashiqurr04@gmail.com>
2022-05-21 23:52:32 +0600

    1. Successfully Collecting indivisual movie data (redirecting)

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,18 +1,67 @@
 
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Text, ActivityIndicator, StyleSheet, View, ScrollView, Image, Dimensions } from 'react-native';
+// import {getMovie} from '../services/Services';
 
+const placeholderImage = require('../assets/Images/placeholder.jpg');
+const height = Dimensions.get('window').height;
 
 
 const Detail = ({ route, navigation }) => {
-    
+
+    // const { movieId } = route.params.movieDetail.id;
+    // const [movieDetail, setMovieDetail] = useState();
+    // const [loaded, setLoaded] = useState(false);
+
+    // useEffect(() => {
+    //     getMovie(movieId).then(movieData => {
+    //         setMovieDetail(movieData);
+    //         setLoaded(true);
+    //     })
+    // }, [movieData]);
+
+
+
+
+    const [loaded, setLoaded] = useState(false);
     const { movieDetail } = route.params;
-    // const { movieDetail } = route.params.movieDetail;
     return (
         <React.Fragment>
-            <Text>{movieDetail.title}</Text>
+            {!loaded && (
+                <ScrollView>
+                    <View>
+                        <Image
+                            resizeMode='cover'
+                            style={styles.image}
+                            source={
+                                movieDetail.poster_path ? { uri: 'https://image.tmdb.org/t/p/w500' + movieDetail.poster_path } : placeholderImage
+                            }
+                        />
+                        <Text style={styles.movieTitle}>{movieDetail.title}</Text>
+                    </View>
+                </ScrollView>
+            )}
+            {loaded && (<ActivityIndicator style={styles.ActivityIndicator} size="large" />)}
         </React.Fragment>
     );
 }
+
+
+const styles = StyleSheet.create({
+    image: {
+        height: height/2,
+    },
+    movieTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    ActivityIndicator: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
 
 export default Detail;
