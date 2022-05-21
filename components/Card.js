4911a3a4d7a7e4a20f5c5cc82f71f659e524734d
Ashiqur Rahman <ashiqurr04@gmail.com>
2022-05-21 17:00:52 +0600

    1. Adding placeholder for when movie image failed to open

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
+
+const placeholderImage = require('../assets/Images/placeholder.jpg');
+
 class Card extends React.PureComponent {
 
     render() {
@@ -10,8 +13,11 @@ class Card extends React.PureComponent {
                 <Image
                     resizeMode='cover'
                     style={styles.image}
-                    source={{ uri: 'https://image.tmdb.org/t/p/w500' + item.poster_path }}
+                    source={
+                        item.poster_path ? { uri: 'https://image.tmdb.org/t/p/w500' + item.poster_path } : placeholderImage
+                    }
                 />
+                {!item.poster_path && <Text style={styles.movieName}>{item.title}</Text>}
             </TouchableOpacity>
         );
     }
@@ -22,11 +28,21 @@ const styles = StyleSheet.create({
     container: {
         padding: 5,
         position: 'relative',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 200,
     },
     image: {
         height: 200,
         width: 120,
         borderRadius: 20,
     },
+    movieName: {
+        position: 'absolute',
+        width: 100,
+        top: 10,
+        textAlign: 'center',
+    }
+
 });
 export default Card;
